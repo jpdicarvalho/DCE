e520f985f5a8d3da8f805dce82932df8ea321d40
jpdicarvalho <joaopedrobraga.07@gmail.com>
2025-03-02 17:16:11 -0300

    add input to get the value kwh

--- frontend/src/SearchSupplier/SearchSupplier.jsx
+++ frontend/src/SearchSupplier/SearchSupplier.jsx
@@ -2,10 +2,13 @@ import './SearchSupplie.css'
 import Logo from '../assets/image.png'
 import image_section from '../assets/image-section.png'
 import { IoIosArrowRoundForward } from "react-icons/io";
+import { useState } from 'react';
 
 
 
 function SearchSupplie () {
+    const [inputEnergia, setInputEnergia] = useState (false)
+
     return (
         <>
             <div className='main__SearchSupplie'>
@@ -15,21 +18,38 @@ function SearchSupplie () {
                     </div>
                 </div>
                 <div className='section__main'>
-                    <div className='get__started'>
-                        <h1 className='text__solution'>Nossas <br/>Soluções</h1>
-                        <p className='text__information'>Confira o serviço de busca de fornecedores da Clarke Energia!
-                            Informe seu consumo mensal de energia e encontre as melhores opções de fornecimento para a sua empresa.
-                        </p>
-                        <button className='btn__get__started'>
-                            <p>Encontrar fornecedor</p>
-                            <IoIosArrowRoundForward className='icon__IoIosArrowRoundForward'/>
+                    {inputEnergia ? (
+                        <>
+                            <div className='input__box'>
+                                <div class="input-wrapper">
+                                    <input placeholder="Informe um valor, ex.: 30000" class="input" name="text" type="text" />
+                                    <button class="Subscribe-btn">Encontrar</button>
+                                </div>
+                            </div>
+                        </>
+                    ):(
+                        <>
+                            <div className='get__started'>
+                                <h1 className='text__solution'>Nossas <br/>Soluções</h1>
+                                <p className='text__information'>Confira o serviço de busca de fornecedores da Clarke Energia!
+                                    Informe seu consumo mensal de energia e encontre as melhores opções de fornecimento para a sua empresa.
+                                </p>
+                                <button className='btn__get__started' onClick={() => {setInputEnergia(true)}}>
+                                    <p>Encontrar fornecedor</p>
+                                    <IoIosArrowRoundForward className='icon__IoIosArrowRoundForward'/>
+                                </button>
+                                <div class="input-wrapper">
+                                    <input placeholder="Informe um valor, ex.: 30000" class="input" name="text" type="text" />
+                                    <button class="Subscribe-btn">Encontrar</button>
+                                </div>  
+                            </div>
 
-                        </button>
-                    </div>
-
-                    <div className='image__section'>
-                        <img className='inner_image_section' src={image_section} alt="" />
-                    </div>
+                            <div className='image__section'>
+                                <img className='inner_image_section' src={image_section} alt="" />
+                            </div>
+                        </>
+                    )}
+                    
                 </div>
             </div>
         </>
