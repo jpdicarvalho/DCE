e5dee8f4e3e01ecab96091babf2f3e2ca47826c1
jpdicarvalho <joaopedrobraga.07@gmail.com>
2025-03-02 17:31:37 -0300

    animation buttons

--- frontend/src/SearchSupplier/SearchSupplier.jsx
+++ frontend/src/SearchSupplier/SearchSupplier.jsx
@@ -3,7 +3,7 @@ import Logo from '../assets/image.png'
 import image_section from '../assets/image-section.png'
 import { IoIosArrowRoundForward } from "react-icons/io";
 import { useState } from 'react';
-
+import { motion } from "framer-motion";
 
 
 function SearchSupplie () {
@@ -18,37 +18,44 @@ function SearchSupplie () {
                     </div>
                 </div>
                 <div className='section__main'>
-                    {inputEnergia ? (
-                        <>
-                            <div className='input__box'>
-                                <div class="input-wrapper">
-                                    <input placeholder="Informe um valor, ex.: 30000" class="input" name="text" type="text" />
-                                    <button class="Subscribe-btn">Encontrar</button>
-                                </div>
-                            </div>
-                        </>
-                    ):(
-                        <>
-                            <div className='get__started'>
-                                <h1 className='text__solution'>Nossas <br/>Soluções</h1>
-                                <p className='text__information'>Confira o serviço de busca de fornecedores da Clarke Energia!
-                                    Informe seu consumo mensal de energia e encontre as melhores opções de fornecimento para a sua empresa.
-                                </p>
-                                <button className='btn__get__started' onClick={() => {setInputEnergia(true)}}>
-                                    <p>Encontrar fornecedor</p>
-                                    <IoIosArrowRoundForward className='icon__IoIosArrowRoundForward'/>
-                                </button>
-                                <div class="input-wrapper">
-                                    <input placeholder="Informe um valor, ex.: 30000" class="input" name="text" type="text" />
-                                    <button class="Subscribe-btn">Encontrar</button>
-                                </div>  
-                            </div>
+                    <div className="get__started">
+                        <h1 className="text__solution">Nossas <br/>Soluções</h1>
+                        <p className="text__information">
+                            Confira o serviço de busca de fornecedores da Clarke Energia! Informe seu consumo
+                            mensal de energia e encontre as melhores opções de fornecimento para a sua empresa.
+                        </p>
 
-                            <div className='image__section'>
-                                <img className='inner_image_section' src={image_section} alt="" />
-                            </div>
-                        </>
-                    )}
+                        {!inputEnergia && (
+                            <button 
+                            className="btn__get__started" 
+                            onClick={() => setInputEnergia(true)}
+                            >
+                            <p>Encontrar fornecedor</p>
+                            <IoIosArrowRoundForward className="icon__IoIosArrowRoundForward" />
+                            </button>
+                        )}
+
+                        {inputEnergia && (
+                            <motion.div 
+                            className="input-wrapper"
+                            initial={{ x: "-100%", opacity: 0 }}
+                            animate={{ x: 0, opacity: 1 }}
+                            transition={{ duration: 0.5, ease: "easeOut" }}
+                            >
+                            <input 
+                                placeholder="Informe um valor, ex.: 30000" 
+                                className="input" 
+                                name="text" 
+                                type="text" 
+                            />
+                            <button className="Search-btn">Encontrar</button>
+                            </motion.div>
+                        )}
+                    </div>
+
+                    <div className='image__section'>
+                        <img className='inner_image_section' src={image_section} alt="" />
+                    </div>
                     
                 </div>
             </div>
