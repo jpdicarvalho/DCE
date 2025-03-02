a52c6c71125a6225c49cdda6ef40e67b0479b269
jpdicarvalho <joaopedrobraga.07@gmail.com>
2025-03-02 13:31:15 -0300

    update style

--- frontend/src/SearchSupplier/SearchSupplier.jsx
+++ frontend/src/SearchSupplier/SearchSupplier.jsx
@@ -1,35 +1,37 @@
 import './SearchSupplie.css'
 import Logo from '../assets/image.png'
 import image_section from '../assets/image-section.png'
-import { FaArrowRightLong } from "react-icons/fa6";
+import { IoIosArrowRoundForward } from "react-icons/io";
 
 
 
 function SearchSupplie () {
     return (
         <>
-           <div className='header__SearchSupplie'>
-                <div className='box__image'>
-                    <img className='logo' src={Logo} alt="" />
+            <div className='main__SearchSupplie'>
+                <div className='header__SearchSupplie'>
+                    <div className='box__image'>
+                        <img className='logo' src={Logo} alt="" />
+                    </div>
                 </div>
-           </div>
-           <div className='section__main'>
-                <div className='get__started'>
-                    <h1>Nossas <br/>Soluções</h1>
-                    <p>Confira o serviço de busca de fornecedores da Clarke Energia!
-                        Informe seu consumo mensal de energia e encontre as melhores opções de fornecimento para a sua empresa.
-                    </p>
-                    <button>
-                        <p>Encontrar fornecedor</p>
-                        <FaArrowRightLong />
+                <div className='section__main'>
+                    <div className='get__started'>
+                        <h1 className='text__solution'>Nossas <br/>Soluções</h1>
+                        <p className='text__information'>Confira o serviço de busca de fornecedores da Clarke Energia!
+                            Informe seu consumo mensal de energia e encontre as melhores opções de fornecimento para a sua empresa.
+                        </p>
+                        <button className='btn__get__started'>
+                            <p>Encontrar fornecedor</p>
+                            <IoIosArrowRoundForward className='icon__IoIosArrowRoundForward'/>
 
-                    </button>
-                </div>
+                        </button>
+                    </div>
 
-                <div className='image__section'>
-                    <img className='inner_image_section' src={image_section} alt="" />
+                    <div className='image__section'>
+                        <img className='inner_image_section' src={image_section} alt="" />
+                    </div>
                 </div>
-           </div>
+            </div>
         </>
     )
 }
