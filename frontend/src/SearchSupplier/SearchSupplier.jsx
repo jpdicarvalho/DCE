26a9c4c37c806902f82e89cf463fa6e5c823fa3f
jpdicarvalho <joaopedrobraga.07@gmail.com>
2025-03-04 14:24:25 -0300

    create card fornecedor

--- frontend/src/SearchSupplier/SearchSupplier.jsx
+++ frontend/src/SearchSupplier/SearchSupplier.jsx
@@ -1,17 +1,54 @@
+import { useQuery, useLazyQuery } from "@apollo/client";
+import { gql } from "../config/apolloClient";
+
 import './SearchSupplie.css'
+
 import Logo from '../assets/image.png'
 import image_section from '../assets/image-section.png'
+import image_example from '../assets/energy.jpg'
+
 import { IoIosArrowRoundForward } from "react-icons/io";
+import { MdOutlineStar } from "react-icons/md";
+import { BsBarChartFill } from "react-icons/bs";
+import { FaUsers } from "react-icons/fa6";
+
 import { useState } from 'react';
 import { motion } from "framer-motion";
 
+const GET_FORNECEDORES = gql`
+    query GetFornecedores($consumo: Int!) {
+        fornecedores(consumo: $consumo) {
+            id
+            nome
+            logo
+            estado
+            custo_por_kwh
+            limite_minimo_kwh
+            avaliacaoMedia
+            clientesAtendidos
+        }
+    }
+`;
+
 
 function SearchSupplie () {
-    const [inputEnergia, setInputEnergia] = useState (false)
+    const [inputEnergia, setInputEnergia] = useState (false);
+    const [valorConsumo, setValorConsumo] = useState ("");
+    const [buscarFornecedores, { data, loading, error }] = useLazyQuery(GET_FORNECEDORES);
+
+
+    const handleBuscarFornecedores = () => {
+        const consumo = parseInt(valorConsumo, 10); // Convertendo string para número
+        if (isNaN(consumo) || consumo <= 0) {
+            alert("Por favor, insira um valor válido de consumo.");
+            return;
+        }
+        buscarFornecedores({ variables: { consumo } });
+    };
 
     return (
         <>
-            <div className='main__SearchSupplie'>
+           <div className='main__SearchSupplie'>
                 <div className='header__SearchSupplie'>
                     <div className='box__image'>
                         <img className='logo' src={Logo} alt="" />
@@ -27,28 +64,35 @@ function SearchSupplie () {
 
                         {!inputEnergia && (
                             <button 
-                            className="btn__get__started" 
-                            onClick={() => setInputEnergia(true)}
+                                className="btn__get__started" 
+                                onClick={() => setInputEnergia(true)}
                             >
-                            <p>Encontrar fornecedor</p>
-                            <IoIosArrowRoundForward className="icon__IoIosArrowRoundForward" />
+                                <p>Encontrar fornecedor</p>
+                                <IoIosArrowRoundForward className="icon__IoIosArrowRoundForward" />
                             </button>
                         )}
 
                         {inputEnergia && (
                             <motion.div 
-                            className="input-wrapper"
-                            initial={{ x: "-100%", opacity: 0 }}
-                            animate={{ x: 0, opacity: 1 }}
-                            transition={{ duration: 0.5, ease: "easeOut" }}
+                                className="input-wrapper"
+                                initial={{ x: "-100%", opacity: 0 }}
+                                animate={{ x: 0, opacity: 1 }}
+                                transition={{ duration: 0.5, ease: "easeOut" }}
                             >
-                            <input 
-                                placeholder="Informe um valor, ex.: 30000" 
-                                className="input" 
-                                name="text" 
-                                type="text" 
-                            />
-                            <button className="Search-btn">Encontrar</button>
+                                <input 
+                                    placeholder="Informe um valor, ex.: 30000" 
+                                    className="input" 
+                                    name="text" 
+                                    type="text" 
+                                    value={valorConsumo}
+                                    onChange={(e) => setValorConsumo(e.target.value)}
+                                />
+                                <button 
+                                    className="Search-btn"
+                                    onClick={handleBuscarFornecedores}
+                                >
+                                    Encontrar
+                                </button>
                             </motion.div>
                         )}
                     </div>
@@ -56,7 +100,64 @@ function SearchSupplie () {
                     <div className='image__section'>
                         <img className='inner_image_section' src={image_section} alt="" />
                     </div>
-                    
+                </div>
+
+                {/* Exibindo os fornecedores */}
+                <div className="result__section">
+                    {loading && <p>Buscando fornecedores...</p>}
+                    {error && <p>Erro ao buscar fornecedores: {error.message}</p>}
+                    {data && data.fornecedores.length === 0 && <p>Nenhum fornecedor encontrado.</p>}
+                    {data && data.fornecedores.map((fornecedor) => (
+                        
+                        <div key={fornecedor.id} className="fornecedor__card">
+                            <div className="box__fornecedor__logo">
+                                <img src={image_example} alt={fornecedor.nome} className="fornecedor__logo"/>
+                            </div>
+
+                            <div className="box__fornecedor__information">
+                                <h4>{fornecedor.nome} ● {fornecedor.estado}</h4>
+                                
+                                <div className="box__rating">
+                                    <MdOutlineStar className="icon__MdOutlineStar"/>
+                                    <p className="text__rating">{fornecedor.avaliacaoMedia.toFixed(1)} (422 avaliações)</p>
+                                </div>
+
+                                <div className="container__insights">
+                                    <div className="box__insights">
+                                        <BsBarChartFill className="icon__insights"/>
+                                        <p className="text__insights">
+                                            Limite mínimo<br/>
+                                            {fornecedor.limite_minimo_kwh} kWh
+                                        </p>
+                                    </div>
+
+                                    <div className="box__insights">
+                                        <FaUsers className="icon__insights"/>
+                                        <p className="text__insights">
+                                            Clientes atendidos<br/>
+                                            {fornecedor.clientesAtendidos}+
+                                        </p>
+                                    </div>
+                                    
+                                </div>
+                                
+                                
+                                <div className="bottom__card">
+                                    <div className="box__value__kwh">
+                                        <h3>R$ {fornecedor.custo_por_kwh.toFixed(2)}</h3>
+                                        <p className="text__kwh">/kWh</p>
+                                    </div>
+                                    
+
+                                    <button>
+                                        aa
+                                    </button>
+                                </div>
+
+                            </div>
+                            
+                        </div>
+                    ))}
                 </div>
             </div>
         </>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ApolloProvider } from "@apollo/client";
+import client from "./config/apolloClient";
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <ApolloProvider client={client}>
+        <StrictMode>
+          <App />
+        </StrictMode>,
+    </ApolloProvider>
+);
